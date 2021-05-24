db8d99944c0e3e2bec9ed12ed1411a849b73ff36
Chu Duc Thang <chuducthang77@gmail.com>
2021-05-24 16:21:36 +0700

    Loading state

--- movieapp/src/components/MovieView.js
+++ movieapp/src/components/MovieView.js
@@ -6,18 +6,26 @@ const MovieView = () => {
 
     const {id} = useParams()
     const [movieDetails, setMovieDetails] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=0ff7dbc96c65ca2ec70347218a338eeb&language=en-US`)
-        .then(reponse => reponse.json())
-        .then(data => setMovieDetails(data))
+        .then(response => response.json())
+        .then(data => setMovieDetails(data),
+                setIsLoading(false))
     }, [id])
 
-  return (
-    <>
-      <Hero text={movieDetails.original_title} />
-    </>
-  );
+    function renderMovieDetails() {
+        if (isLoading) {
+            return <Hero text="..."/>
+        }
+
+        if (movieDetails) {
+            return <Hero text={movieDetails.original_title} />
+        }
+    }
+
+  return renderMovieDetails()
 };
 
 export default MovieView;
