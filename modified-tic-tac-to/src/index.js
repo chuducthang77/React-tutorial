011bb33fd6af76aaa6573104465cec9a0bcfd224
Chu Duc Thang <chuducthang77@gmail.com>
2021-05-26 11:28:34 +0700

    Added new simple implementation of tic-tac-to

--- modified-tic-tac-to/src/index.js
+++ modified-tic-tac-to/src/index.js
@@ -18,6 +18,7 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
       isX: true,
+      depth: 9,
     };
   }
 
@@ -28,12 +29,19 @@ class Board extends React.Component {
       return;
     }
     if (this.state.isX) {
-        squares[i] = 'X'
+      squares[i] = "X";
     } else {
-        const number = Math.floor(Math.random() * 9)
-        squares[number] = 'O'
+      //   let number = Math.floor(Math.random() * 9)
+      //   while (squares[number]) {
+      //       number = Math.floor(Math.random() * 9)
+      //   }
+      let number = MiniMax(this.state.squares, this.state.depth, true);
+      squares[number] = "O";
     }
-    this.setState({ squares: squares, isX: !this.state.isX });
+    let num = this.state.depth
+    num--
+    this.setState({ squares: squares, isX: !this.state.isX,
+                    depth: num});
   }
 
   renderSquare(i) {
@@ -49,7 +57,7 @@ class Board extends React.Component {
     let status;
     const winner = calculateWinner(this.state.squares);
     if (winner) {
-      status = "Winner: " + winner;
+      status = "Result: " + winner;
     } else {
       status = "Next player: " + (this.state.isX ? "X" : "O");
     }
@@ -77,22 +85,22 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" };
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {value: ''};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  //     this.handleChange = this.handleChange.bind(this);
+  //     this.handleSubmit = this.handleSubmit.bind(this);
+  //   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+  //   handleChange(event) {
+  //     this.setState({value: event.target.value});
+  //   }
 
-  handleSubmit(event) {
-      
-    event.preventDefault();
-  }
+  //   handleSubmit(event) {
+  //     return <Board />
+  //     event.preventDefault();
+  //   }
 
   render() {
     return (
@@ -100,8 +108,13 @@ class Game extends React.Component {
         <div className="game-board">
           <Board />
         </div>
-        <div className="game-info">
-        </div>
+        {/* <form onSubmit={this.handleSubmit}>
+        <label>
+          Enter the square you want to play:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form> */}
       </div>
     );
   }
@@ -122,11 +135,75 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+  let draw = true;
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
+    if (!squares[i]) {
+      draw = false;
+    }
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
+  if (draw) {
+    return "draw";
+  }
   return null;
+}
+
+function MiniMax(squares, depth, maximizingPlayer) {
+  
+  if (depth === 0) {
+    const result = calculateWinner(squares);
+    if (result === "X") {
+      return -1;
+    } else if (result === "O") {
+      return 1;
+    } else if (result === "draw") {
+      return 0;
+    }
+  }
+
+  if (maximizingPlayer) {
+
+    let value = -Infinity;
+    let position = Array(depth).fill(null);
+    let j = 0;
+
+    //Save the position is still empty in the board
+    for (let i = 0; i < squares.length; i++) {
+      if (squares[i] == null) {
+        position[j] = i;
+        j++;
+      }
+    }
+    
+    //For each position, fill in that position and calculate the value it returns
+    for (let i = 0; i < position.length; i++) {
+      let tempSquares = squares.slice()
+      tempSquares[position[i]] = "O";
+      let temp = MiniMax(tempSquares, depth - 1, false)
+      value = Math.max(value, temp);
+      
+    }
+    
+  } else {
+    let value = +Infinity;
+    let position = Array(depth).fill(null);
+    let j = 0;
+
+    //Save the position is still empty in the board
+    for (let i = 0; i < squares.length; i++) {
+      if (squares[i] == null) {
+        position[j] = i;
+        j++;
+      }
+    }
+    //For each position, fill in that position and calculate the value it returns
+    for (let i = 0; i < position; i++) {
+      squares[position[i]] = "X";
+      value = Math.min(value, MiniMax(squares, depth - 1, true));
+      return value;
+    }
+  }
 }
